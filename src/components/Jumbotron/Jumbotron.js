3998a4dbb8ac0d7879ac45420d0f0791fb4ecdc1
Adam Chernick <jchernick2010@gmail.com>
2018-10-27 18:32:10 -0400

    added logic

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -4,6 +4,7 @@ import "./Jumbotron.css";
 const Jumbotron = props => (
   <div className="jumbotron text-center">
     <h1>Score: {props.score}</h1>
+    <h6>Click on the characters but don't forget who you have clicked on! Try to get them all!</h6>
   </div>
 );
 
