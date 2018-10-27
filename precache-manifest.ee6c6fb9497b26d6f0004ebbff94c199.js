9d643ed78ac1b933c7066328bae37fae7948b097
Adam Chernick <jchernick2010@gmail.com>
2018-10-27 18:32:46 -0400

    Updates

---
+++ precache-manifest.ee6c6fb9497b26d6f0004ebbff94c199.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "794047f1fffdfe5ffde3",
+    "url": "/React-Memory-Game/static/js/runtime~main.794047f1.js"
+  },
+  {
+    "revision": "09445a032e2718367f92",
+    "url": "/React-Memory-Game/static/js/main.09445a03.chunk.js"
+  },
+  {
+    "revision": "7ccad06e8a96e8ae6253",
+    "url": "/React-Memory-Game/static/js/1.7ccad06e.chunk.js"
+  },
+  {
+    "revision": "09445a032e2718367f92",
+    "url": "/React-Memory-Game/static/css/main.5d118a71.chunk.css"
+  },
+  {
+    "revision": "17b1270bc053c4c0d78dbdc433e88e27",
+    "url": "/React-Memory-Game/index.html"
+  }
+];
